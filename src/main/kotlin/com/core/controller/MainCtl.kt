2e0ad48554664208f33af9a9838e023023935e65
DaoNz <ionmax5602@naver.com>
2024-06-28 11:03:45 +0900

    Feat : add sample child controller option

--- src/main/kotlin/com/core/controller/MainCtl.kt
+++ src/main/kotlin/com/core/controller/MainCtl.kt
@@ -8,6 +8,7 @@ import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.CornerRadii
 import javafx.scene.paint.Color
 import javafx.geometry.Insets
+import javafx.scene.shape.Rectangle
 import java.net.URL
 import java.util.*
 
@@ -16,10 +17,15 @@ class MainCtl : Initializable {
     @FXML
     private lateinit var pane : AnchorPane
 
+    @FXML private lateinit var rect01 : Rectangle
+    @FXML private lateinit var rect02 : Rectangle
+
     override fun initialize(loc: URL?, res: ResourceBundle?) {
         pane.minHeight = 600.0;
         pane.minWidth = 900.0;
         pane.background = Background(BackgroundFill(Color.GREEN, CornerRadii(10.0), Insets(5.0)))
 
+        rect01.isVisible = true
+        rect02.isVisible = false
     }
 }
