c0d4b335c01ffc1de18e643f8a0cdce4eda8d34b
DaoNz <ionmax5602@naver.com>
2024-07-02 20:24:19 +0900

    Feat : add sample btn action logic

--- src/main/kotlin/com/core/controller/MainCtl.kt
+++ src/main/kotlin/com/core/controller/MainCtl.kt
@@ -8,17 +8,17 @@ import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.CornerRadii
 import javafx.scene.paint.Color
 import javafx.geometry.Insets
+import javafx.scene.control.Button
 import javafx.scene.shape.Rectangle
 import java.net.URL
 import java.util.*
 
 class MainCtl : Initializable {
 
-    @FXML
-    private lateinit var pane : AnchorPane
-
+    @FXML private lateinit var pane : AnchorPane
     @FXML private lateinit var rect01 : Rectangle
     @FXML private lateinit var rect02 : Rectangle
+    @FXML private lateinit var btnGo : Button
 
     override fun initialize(loc: URL?, res: ResourceBundle?) {
         pane.minHeight = 600.0
@@ -27,5 +27,12 @@ class MainCtl : Initializable {
 
         rect01.isVisible = true
         rect02.isVisible = false
+
+        btnGo.setOnMouseClicked { btn_onClick() }
+    }
+
+    fun btn_onClick() {
+        println("call new scene")
+        //TODO - call new scene
     }
 }
